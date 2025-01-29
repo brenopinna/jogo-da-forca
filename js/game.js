bf313aa636e511db54824fae4f3858eabdd9913a
brenopinna <brenopinna1008@gmail.com>
2025-01-29 17:13:19 -0300

    commit especial integracao focus

--- js/game.js
+++ js/game.js
@@ -1,106 +1,141 @@
-const containerTema = document.querySelector('.tema');
-const input = document.getElementById('chute');
-const containerChutesRestantes = document.getElementById('chutes-restantes');
-const containerChutesAnteriores = document.getElementById('chutes-anteriores');
-const palavraContainer = document.querySelector('.palavra');
-const situacaoContainer = document.querySelector('.situacao');
-const respostaContainer = document.querySelector('.resposta');
-const buttons = document.querySelectorAll('button');
+const containerTema = document.querySelector(".tema")
+const input = document.getElementById("chute")
+const containerChutesRestantes = document.getElementById("chutes-restantes")
+const containerChutesAnteriores = document.getElementById("chutes-anteriores")
+const palavraContainer = document.querySelector(".palavra")
+const situacaoContainer = document.querySelector(".situacao")
+const respostaContainer = document.querySelector(".resposta")
+const buttons = document.querySelectorAll("button")
 
-const alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v', 'w', 'x', 'y', 'z']
+input.focus()
 
-const tema = JSON.parse(localStorage.getItem('tema'));
-const palavra = retornarPalavraAleatoria(tema);
+const alfabeto = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+]
 
-containerTema.innerText = capitalize(tema.nome);
+function removerAcentos(str) {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+}
 
-inserirSpans(palavra);
+const tema = JSON.parse(localStorage.getItem("tema"))
+const palavra = retornarPalavraAleatoria(tema)
 
-const spans = palavraContainer.querySelectorAll('span');
+containerTema.innerText = capitalize(tema.nome)
 
-input.addEventListener('keypress', (e) => {
-   if(e.key == 'Enter'){
-      const inputValue = input.value.toLowerCase();
-      input.value = '';
-      if(inputValue.length == 1 && alfabeto.includes(inputValue)){
-         verificaAcerto(inputValue, palavra);
-         if(defineVitoria()){
-            finalizaJogo('VOCÊ GANHOU!!')
-         }else if(defineDerrota()){
-            finalizaJogo('PERDEU!!!!')
-            respostaContainer.innerHTML = `A palavra secreta era "${palavra}"`;
-         }
-      }else{
-         alert('Insira uma única letra, sem acentuação!')
+inserirSpans(palavra)
+
+const spans = palavraContainer.querySelectorAll("span")
+
+input.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    const inputValue = input.value.toLowerCase()
+    input.value = ""
+    if (inputValue.length == 1 && alfabeto.includes(inputValue)) {
+      verificaAcerto(inputValue, palavra)
+      if (defineVitoria()) {
+        finalizaJogo("VOCÊ GANHOU!!")
+      } else if (defineDerrota()) {
+        finalizaJogo("PERDEU!!!!")
+        respostaContainer.innerHTML = `A palavra secreta era "${palavra}"`
       }
-   }
-});
+    } else {
+      alert("Insira uma única letra, sem acentuação!")
+    }
+  }
+})
 
-function retornarPalavraAleatoria(tema){
-   const palavras = tema.palavras;
-   const randomIndex = Math.floor(Math.random() * palavras.length);
-   
-   return palavras[randomIndex];
+function retornarPalavraAleatoria(tema) {
+  const palavras = tema.palavras
+  const randomIndex = Math.floor(Math.random() * palavras.length)
+
+  return palavras[randomIndex]
 }
 
-function inserirSpans(palavra){
-   for(let letra of palavra){
-      const span = document.createElement('span');
-      if(letra == '-'){
-         span.innerHTML = '-';
-      }else if(letra == ' '){
-         span.innerHTML = '';
-         span.style.margin = '0 0.5rem'
-      }else{
-         span.innerHTML = '_';
+function inserirSpans(palavra) {
+  for (let letra of palavra) {
+    const span = document.createElement("span")
+    span.style.margin = "0 0.2rem"
+    if (letra == "-") {
+      span.innerHTML = "-"
+    } else if (letra == " ") {
+      span.innerHTML = " "
+      span.style.margin = "0 0.3rem"
+    } else {
+      span.innerHTML = "_"
+    }
+    palavraContainer.append(span)
+  }
+}
+
+function verificaAcerto(userInput, palavra) {
+  let certo = false
+  let chutes = containerChutesAnteriores.innerHTML
+  const palavraSemAcento = removerAcentos(palavra)
+  if (!chutes.includes(userInput)) {
+    for (let i = 0; i < palavra.length; i++) {
+      if (userInput == palavraSemAcento[i].toLowerCase()) {
+        spans[i].innerHTML = palavra[i].toLowerCase()
+        certo = true
       }
-      palavraContainer.append(span);
-   }
+    }
+    if (!certo) {
+      containerChutesRestantes.innerHTML -= 1
+      containerChutesAnteriores.innerHTML += `${userInput} `
+    }
+  } else {
+    alert(`A letra ${userInput} já foi chutada!`)
+  }
 }
 
-function verificaAcerto(userInput, palavra){
-   let certo = false;
-   let chutes = containerChutesAnteriores.innerHTML;
-   if(!chutes.includes(userInput)){
-      for(let i = 0; i < palavra.length; i++){
-         if(userInput == palavra[i]){
-            spans[i].innerHTML = userInput;
-            certo = true;
-         }
-      };
-      if(!certo){
-         containerChutesRestantes.innerHTML -=  1;
-         containerChutesAnteriores.innerHTML +=`${userInput} `;
-      }
-   }else{
-      alert(`A letra ${userInput} já foi chutada!`)
-   }
+function defineVitoria() {
+  let palavraFormada = ""
+  spans.forEach((span) => {
+    palavraFormada += span.innerText
+  })
+  return palavra.toLowerCase() == palavraFormada
 }
 
-function defineVitoria(){
-   let palavraFormada = '';
-   spans.forEach(span => {
-      palavraFormada += span.innerText;
-   });
-   return palavra == palavraFormada;
+function defineDerrota() {
+  return containerChutesRestantes.innerHTML == 0
 }
 
-function defineDerrota(){
-   return containerChutesRestantes.innerHTML == 0
+function finalizaJogo(msg) {
+  mostraBotoes()
+  situacaoContainer.innerHTML = msg
+  input.disabled = true
 }
 
-function finalizaJogo(msg){
-   mostraBotoes();
-   situacaoContainer.innerHTML = msg;
-   input.disabled = true;
+function mostraBotoes() {
+  buttons.forEach((button) => {
+    button.style.display = "block"
+  })
 }
 
-function mostraBotoes(){
-   buttons.forEach(button => {
-      button.style.display = 'block';
-   })
-}
-
-function capitalize(str){
-   return str.charAt(0).toUpperCase() + str.slice(1);
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
--- js/temas.js
+++ js/temas.js
@@ -1,81 +1,97 @@
-const optionsContainer = document.querySelector('.options-container');
-const nextPageLink = document.querySelector('a')
+const optionsContainer = document.querySelector(".options-container")
+const nextPageLink = document.querySelector("a")
 
 // OBS: Registrar os temas sem caracteres especiais!!
 const temas = {
-   'alimentos' : ['maca','banana','pera', 'carne'],
-   'profissões' : ['medico','advogado','programador','professor','dentista'],
-   'países' : ['brasil','mexico','argentina','espanha','estados-unidos','alemanha'],
-   'animais' : ['leao','macaco','mico-leao-dourado','jacare','arara']
+  "palavra 1": ["Bench"],
+  "palavra 2": ["Caldeirão"],
+  "palavra 3": ["Lead"],
+  "palavra 4": ["Selo EJ"],
+  "palavra 5": ["Sabatina"],
+  "palavra 6": ["Guardião"],
+  "palavra 7": ["Liderança"],
+  "palavra 8": ["Meta"],
+  "palavra 9": ["Reunião de Diagnóstico"],
+  "palavra 10": ["EFEJ"],
+  "palavra 11": ["Processo Seletivo Personalizado"],
 }
 
-criaInputs(temas);
+criaInputs(temas)
 
-const inputs = document.querySelectorAll('input[name=opcao]');
+const inputs = document.querySelectorAll("input[name=opcao]")
 
-const localStorageTheme = localStorage.length > 0 ? JSON.parse(localStorage.getItem('tema')).nome : false;
+const localStorageTheme =
+  localStorage.length > 0 ? JSON.parse(localStorage.getItem("tema")).nome : false
 
-if(localStorageTheme){
-   for(const input of inputs){
-      if(input.value == localStorageTheme){
-         input.checked = true;
+if (localStorageTheme) {
+  for (const input of inputs) {
+    if (input.value == localStorageTheme) {
+      input.checked = true
+    }
+  }
+}
+
+nextPageLink.addEventListener("click", (e) => {
+  let checked = false
+
+  for (const input of inputs) {
+    if (input.checked) {
+      checked = true
+
+      const temaName = input.value
+      const tema = {
+        nome: temaName,
+        palavras: temas[temaName],
       }
-   }
+
+      localStorage.setItem("tema", JSON.stringify(tema))
+    }
+  }
+  if (!checked) {
+    e.preventDefault()
+    alert("Selecione um tema!!")
+  }
+})
+
+function criaInputs(temas) {
+  for (const tema in temas) {
+    const nome = tema
+
+    const inputContainer = retornarInputContainer()
+
+    const inputRadio = retornarInputRadio(nome)
+
+    inputContainer.appendChild(inputRadio)
+    inputContainer.innerHTML += capitalize(nome)
+
+    optionsContainer.appendChild(inputContainer)
+  }
 }
 
-nextPageLink.addEventListener('click', (e) => {
-   let checked = false;
+function retornarInputContainer() {
+  const inputContainer = document.createElement("label")
+  inputContainer.style.display = "block"
+  inputContainer.classList.add("input-container")
+  inputContainer.addEventListener("mouseover", () => {
+    inputContainer.style.backgroundColor = "rgb(210,210,210)"
+  })
+  inputContainer.addEventListener("mouseout", () => {
+    inputContainer.style.backgroundColor = ""
+  })
 
-   for(const input of inputs){
-      if(input.checked){
-         checked = true;
-
-         const temaName = input.value;
-         const tema = {
-            nome : temaName,
-            palavras : temas[temaName]
-         };
-
-         localStorage.setItem('tema', JSON.stringify(tema));
-      }
-   }if(!checked){
-      e.preventDefault();
-      alert('Selecione um tema!!')
-   }
-});
-
-function criaInputs(temas){
-   for(const tema in temas){
-      const nome = tema;
-
-      const inputContainer = retornarInputContainer();
-
-      const inputRadio = retornarInputRadio(nome);
-
-      inputContainer.appendChild(inputRadio);
-      inputContainer.innerHTML += capitalize(nome);
-
-      optionsContainer.appendChild(inputContainer);
-   }
+  return inputContainer
 }
 
-function retornarInputContainer(){
-   const inputContainer = document.createElement('div');
-   inputContainer.classList.add('input-container');
+function retornarInputRadio(id) {
+  const inputRadio = document.createElement("input")
+  inputRadio.setAttribute("type", "radio")
+  inputRadio.setAttribute("name", "opcao")
+  inputRadio.setAttribute("id", id)
+  inputRadio.setAttribute("value", id)
 
-   return inputContainer;
+  return inputRadio
 }
 
-function retornarInputRadio(id){
-   const inputRadio = document.createElement('input');
-   inputRadio.setAttribute('type', 'radio');
-   inputRadio.setAttribute('name', 'opcao');
-   inputRadio.setAttribute('id', id);
-   inputRadio.setAttribute('value', id);
-
-   return inputRadio;
-}
-
-function capitalize(str){
-   return str.charAt(0).toUpperCase() + str.slice(1);
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
